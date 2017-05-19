e891cf360661adf8c19f2026e0bccfc230c76bea
Placid Rodrigues <placidrod@gmail.com>
2017-05-18 19:51:12 -0700

    Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,34 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  var pointer = 0;
+  var dqKey = 0;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    pointer++;
+    list[pointer] = node;
+    list.tail = list[pointer];
+    if(list.head === null) {
+      list.head = list[pointer];
+    }
   };
 
   list.removeHead = function() {
+    dqKey++;
+    var temp = list[dqKey];
+    delete list[dqKey];
+    list.head = list[dqKey+1];
+    return temp.value;
   };
 
   list.contains = function(target) {
+    for(var key in list) {
+      if(list[key].value === target) {
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
@@ -27,3 +47,4 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
