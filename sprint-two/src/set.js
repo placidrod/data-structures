288eb3684fc11fe895d0ba94a8dd480451b650e1
Placid Rodrigues <placidrod@gmail.com>
2017-05-20 09:49:02 -0700

    Complete set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,19 +1,25 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
+//complexity: O(1)
 
 setPrototype.contains = function(item) {
+  return this._storage.hasOwnProperty(item);
 };
+//complexity: O(1)
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
+//complexity: O(1)
 
 /*
  * Complexity: What is the time complexity of the above functions?
