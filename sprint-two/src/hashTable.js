68705e62c517cda1403c7ad1cf366591d2669da4
Placid Rodrigues <placidrod@gmail.com>
2017-05-20 15:33:24 -0700

    Complete hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,16 +5,44 @@ var HashTable = function() {
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v) {
+HashTable.prototype.insert = function(k, v) { //[y, c]
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var exists = false;
+  if(!this._storage.get(index)) {
+    var bucket = [];
+  } else {
+    var bucket = this._storage.get(index);
+    for(var i = 0; i < bucket.length; i++) {
+      if(bucket[i][0] === k) {
+        bucket[i][1] = v;
+        exists = true;
+      }
+    }
+  }
+  if(exists === false) {
+    bucket.push([k, v]);
+  }
+  this._storage.set(index, bucket);
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket  = this._storage.get(index);
+  for(var i = 0; i < bucket.length; i++) {
+    if(bucket[i][0] === k) {
+      return bucket[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var bucket  = this._storage.get(index);
+  for(var i = 0; i < bucket.length; i++) {
+    if(bucket[i][0] === k) {
+      bucket.splice(i,1);
+    }
+  }
 };
 
 
